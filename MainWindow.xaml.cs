90893601a9499c1576533287dce70857f800b22f
Алексей Родионов <mefisto1223@gmail.com>
2022-10-30 16:06:47 +0300

    login

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,17 +14,124 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Npgsql;
 using MaterialDesignThemes.Wpf;
+using System.Diagnostics;
+using System.Data;
 
 namespace Pikachu
 {
+    using BCrypt.Net;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isOpen = false;
+        int iLogin = -1;
+        List<int> names_key = new();
+        List<string> names_title = new();
+        List<string> names_rights = new();
+        List<string> names_pass = new();
+        NpgsqlCommand? iQuery;
+        NpgsqlConnection iConnect = new("Server=127.0.0.1;Port=5432;User Id=postgres;Password=Scorp610;Database=pikachu");
         public MainWindow()
         {
+            Conn_init(iConnect);
+            login(0, "");
             InitializeComponent();
         }
+        private async void Conn_init(NpgsqlConnection iConnect) //соединение с БД, обработка ошибок, обработка изменения состояния соединения
+        {
+            iConnect.StateChange += (object sender, StateChangeEventArgs e) =>
+            {
+                if (!(e.CurrentState == ConnectionState.Open))
+                {
+                    isOpen = false;
+                }
+                else { isOpen = true; }
+            };
+            try
+            {
+                iConnect.Open();
+                string sql = "SELECT * FROM names;";
+                iQuery = new(sql, iConnect);
+                var reader = await iQuery.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    names_key.Add(reader.GetInt32(0));
+                    names_title.Add(reader.GetString(1));
+                    names_rights.Add(reader.GetString(2));
+                    names_pass.Add(reader.GetString(3));
+                }
+                await reader.CloseAsync();
+                iQuery.Dispose();
+
+            }
+            catch (NpgsqlException e)
+            {
+                if (e.Message.Contains("28P01"))
+                {
+                    MessageBox.Show("Неверный логин/пароль БД");
+                    isOpen = false;
+                }
+                else if (e.Message.Contains("3D000"))
+                {
+                    MessageBox.Show("Неверное имя базы данных");
+                    isOpen = false;
+                }
+                else
+                {
+                    MessageBox.Show(e.Message);
+                    isOpen = false;
+                }
+
+            }
+        }
+
+        public void login(int b, string l)
+        {
+            if (!isOpen) { return; } //проверка есть ли соединение
+            Window1 log1 = new();
+            switch (b)
+            {
+                case 1:
+                HintAssist.SetHelperText(log1.PassBox, "Неверный пароль");
+                log1.PassBox.Foreground = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+                log1.PassBox.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+                    log1.LoginBox.Text = l;
+                    break;
+
+                case 2:
+                HintAssist.SetHelperText(log1.LoginBox, "Неверный логин");
+                log1.LoginBox.Foreground = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+                log1.LoginBox.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+                    log1.LoginBox.Text = l;
+                    break;
+            }
+            if (log1.ShowDialog() == true)
+            {                
+                int i=names_title.FindIndex(p => p == log1.Login);
+                if (i > -1)
+                {
+                    if (BCrypt.Verify(log1.Password, names_pass[i]))
+                    {
+                        iLogin = names_key[i];
+                    } else
+                    { 
+                        iLogin = -1;
+                        login(1,log1.Login);
+                    }
+                } 
+                else
+                {
+                    iLogin = -1;
+                    login(2, log1.Login);
+                }
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
     }
 }
---
+++ Window1.xaml.cs
@@ -0,0 +1,97 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pikachu
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private readonly Brush stand;
+        public Window1()
+        {
+            InitializeComponent();
+            LoginIcon.Foreground = LoginBox.BorderBrush;
+            Passicon.Foreground = PassBox.BorderBrush;
+            LoginBox.Foreground = LoginBox.BorderBrush;
+            PassBox.Foreground = PassBox.BorderBrush;
+            stand = PassBox.BorderBrush;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+        public string Password
+        {
+            get { return PassBox.Password; }
+        }
+        public string Login
+        {
+            get { return LoginBox.Text; }
+        }
+        private void LoginBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            LoginIcon.Foreground = (Brush)LoginIcon.FindResource("PrimaryHueMidBrush");
+            HintAssist.SetHelperText(LoginBox, "");
+            LoginBox.Foreground = stand;
+            LoginBox.BorderBrush = (Brush)LoginBox.FindResource("PrimaryHueMidBrush");
+        }
+
+        private void LoginBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginBox.Text == "")
+            {
+                HintAssist.SetHelperText(LoginBox, "Поле не может быть пустым");
+                LoginBox.Foreground = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+                LoginBox.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+
+            }
+            else
+            {
+                LoginBox.Foreground = stand;
+                LoginBox.BorderBrush = stand;
+            }
+            LoginIcon.Foreground = LoginBox.BorderBrush;
+        }
+
+        private void PassBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PassBox.Password == "")
+            {
+                HintAssist.SetHelperText(PassBox, "Поле не может быть пустым");
+                PassBox.Foreground = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+                PassBox.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 98, 80));
+
+            }
+            else
+            {
+                PassBox.Foreground = stand;
+                PassBox.BorderBrush = stand;
+            }
+            Passicon.Foreground = PassBox.BorderBrush;
+        }
+
+        private void PassBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Passicon.Foreground = (Brush)Passicon.FindResource("PrimaryHueMidBrush");
+            HintAssist.SetHelperText(PassBox, "");
+            PassBox.Foreground = stand;
+            PassBox.BorderBrush = (Brush)PassBox.FindResource("PrimaryHueMidBrush");
+        }
+    }
+}
